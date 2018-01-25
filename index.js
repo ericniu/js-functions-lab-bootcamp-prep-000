a6894b2daeefdd4655a39da7b7b5cb3155088564
Eric Niu <eric@goswaggle.com>
2018-01-25 22:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
